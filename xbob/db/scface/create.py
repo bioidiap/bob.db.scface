34597efe340c49356eee2ad11d72aa17ac3334f0
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2012-09-21 12:48:27 +0200

    Try to use nolock/readonly session

--- xbob/db/scface/create.py
+++ xbob/db/scface/create.py
@@ -194,7 +194,7 @@ def create(args):
 
   # the real work...
   create_tables(args)
-  s = session(args.type, args.files[0], echo=(args.verbose >= 2)) 
+  s = session_try_nolock(args.type, args.files[0], echo=(args.verbose >= 2)) 
   add_clients(s, args.featuresfile)
   add_subworlds(s)
   add_files(s, args.imagedir)

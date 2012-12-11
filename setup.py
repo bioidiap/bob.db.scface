f30de32e570d5f63db53cd71430a3394b71e43a1
Manuel Guenther <manuel.guenther@idiap.ch>
2012-12-11 16:48:18 +0100

    Changed version to 1.0.0 to upload package to PyPI.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.scface',
-    version='1.0.0a1',
+    version='1.0.0',
     description='SCface Database Access API for Bob',
     url='http://github.com/bioidiap/xbob.db.scface',
     license='GPLv3',
